94faf8d31b5dc1a852a1b21f43dd4d5c730e7ca9
Martin Hradil <mhradil@redhat.com>
2021-01-23 22:02:27 +0000

    wallpapers/.like.sh - switch from sqlite to per-machine byte counters

--- wallpapers/.like.sh
+++ wallpapers/.like.sh
@@ -13,18 +13,8 @@ md5=`md5sum "$file" | awk '{ print $1 }'`
 echo md5 = "$md5"
 
 cd `dirname "$0"`
-db='.likes.sqlite3'
-if ! [ -f "$db" ]; then
-	# create db
-	(echo "CREATE TABLE [likes] ("
-	 echo "    [md5] VARCHAR(32) NOT NULL,"
-	 echo "    [likes] INTEGER NOT NULL DEFAULT 0,"
-	 echo "    PRIMARY KEY ([md5])"
-	 echo ");"
-	) | sqlite3 "$db"
-fi
+db=".likes.`hostname -s`.$md5"
+echo >> "$db"
 
 echo -n 'likes = '
-(echo 'INSERT OR IGNORE INTO [likes] ([md5]) VALUES ("'"$md5"'");'
- echo 'UPDATE [likes] SET likes = likes + 1 WHERE [md5] = "'"$md5"'";'
- echo 'SELECT [likes] FROM [likes] WHERE [md5] = "'"$md5"'";') | sqlite3 "$db"
+wc -l .likes.*."$md5" | awk '{ print $1 }' | ( sumlines || cat )
