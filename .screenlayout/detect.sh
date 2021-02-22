34b631c5527f6620a0a7b8793531f6d857dcfaf7
Martin Hradil <mhradil@redhat.com>
2021-02-22 23:41:28 +0000

    screenlayout detect: add hdmi-2

--- .screenlayout/detect.sh
+++ .screenlayout/detect.sh
@@ -3,6 +3,8 @@ if xrandr-output-connected 'DP-2-1' && xrandr-output-connected 'DP-2-2'; then
   ~/.screenlayout/set.sh DP-2-1 DP-2-2 eDP-1
 else if xrandr-output-connected 'DP-2-2'; then
   ~/.screenlayout/set.sh DP-2-2 eDP-1
+else if xrandr-output-connected 'HDMI-2'; then
+  ~/.screenlayout/set.sh HDMI-2 eDP-1
 else
   ~/.screenlayout/set.sh eDP-1
-fi; fi
+fi; fi; fi
