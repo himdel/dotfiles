a3d73a6463901d5dd4580e23146cf4ef95a0f18f
Martin Hradil <mhradil@redhat.com>
2021-03-28 19:41:36 +0000

    vim-wrapper: drop ruby specifics, drop File::Globstar

--- bin/vim-wrapper.pl
+++ bin/vim-wrapper.pl
@@ -1,15 +1,14 @@
 #!/usr/bin/env perl
 use v5.14;
 use Carp;
-use Data::Dumper;
-use File::Globstar qw(globstar);
 
 my $VIM='/usr/bin/vim';
 
+# /a/foo (or b) -> GIT_ROOT/foo
 sub gitfind {
 	$_ = shift;
 
-	return $_ unless m#^[ab]/# or m#^vmdb/#;
+	return $_ unless m#^[ab]/#;
 	return $_ if -e $_;
 
 	my $f = s#^[ab]/##r;
@@ -25,31 +24,7 @@ sub gitfind {
 	return ();
 }
 
-sub railsfind {
-	$_ = shift;
-
-	return $_ unless m#^([A-Z]\w+)(\#(\w+))?$#;
-	return $_ if -e $_;
-
-	my ($class, $func) = ($1, $3);
-
-	my $file = $class;
-	$file =~ s/^[A-Z]/\l$&/;
-	$file =~ s/[A-Z]/_\l$&/g;
-	$file =~ s/$/.rb/;
-
-	my @files = globstar("**/$file");
-	if ($func) {
-		@files = map { ($_, '+/def ' . $func . '/') } @files;
-	}
-	return @files;
-
-	carp "railsfind: $_ not found";
-	return ();
-}
-
-my @a = map { /^(.*):(\d+)(:in)?$/ ? ($1, "+$2") : $_ } @ARGV;
+my @a = map { /^(.*):(\d+)(:.*)?$/ ? ($1, "+$2") : $_ } @ARGV;
 @a = map { gitfind $_ } @a;
-@a = map { railsfind $_ } @a;
 
 exec ($VIM, @a);
