cb6a93e725687f5b7da7f4281c2684df461410ee
Martin Hradil <mhradil@redhat.com>
2022-07-19 01:16:25 +0000

    drop miq

--- ManageIQ/script-backups/martinpovolny_manageiq-utils/README.md
+++
@@ -1,2 +0,0 @@
-# manageiq-utils
-Scripts and bits to help with ManageIQ daily dev work
--- ManageIQ/script-backups/martinpovolny_manageiq-utils/miq-add-vsphere.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MIQ_HOST=${MIQ_HOST:-http://localhost:4000/}
-
-. ~/miq-cred.sh
-
-curl --insecure -u admin:smartvm -X POST $MIQ_HOST/api/providers -H "Content-Type: application/json" -H "Accept: application/json" -d '{"type":"ManageIQ::Providers::Vmware::InfraManager","name":"VSP","hostname":"'"$VSP_IP"'","ipaddress":"'"$VSP_IP"'","credentials":[{"userid":"'"$VSP_USER"'","password":"'"$VSP_PASSWORD"'"}]}'
-
