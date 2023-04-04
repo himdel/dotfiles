b20bcf12d77c6d62ab78f6986bc7bb903aa4cd30
Martin Hradil <mhradil@redhat.com>
2023-04-04 01:33:49 +0000

    aule updates

--- .screenlayout/set.sh
+++ .screenlayout/set.sh
@@ -9,7 +9,11 @@ cmd=(xrandr)
 while [ $# -gt 0 ]; do
 	output="$1"; shift
 
-	cmd+=( --output "$output" --mode "$W"x"$H" --pos "$left"x"$top" --rotate "normal" )
+	HH="$H"
+	if [ $output = 'HDMI-2' ]; then HH=2160; fi
+	# TODO WW=1920 or 3840, depending on actual available modes
+
+	cmd+=( --output "$output" --mode "$W"x"$HH" --pos "$left"x"$top" --rotate "normal" )
 	left=$((left + W))
 done
 
