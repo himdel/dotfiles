5204a1a6b9504bd364e19ac74a1a8065498bc366
Martin Hradil <himdel@gmail.com>
2026-09-04 20:59:49 +0000

    wallpapers - back up LastChoosenWallpaper, drop niniel case

--- wallpapers/.set.sh
+++ wallpapers/.set.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-CMD=hsetroot
-
-if [ `hostname -s` = 'niniel' -a `xrandr | grep ' connected' | wc -l` -eq 1 ]; then
-  CMD=/usr/bin/hsetroot
-fi
 
 WALL="$1"
+hsetroot -fill "$WALL"
+
+cp -a ~/.fvwm/preferences/LastChoosenWallpaper{,~}
 fullpath=$(cd "$(dirname "$WALL")" ; pwd)/$(basename "$WALL")
-
-"$CMD" -fill "$WALL"
-
 echo "Wallpaper-Set '$fullpath'" > ~/.fvwm/preferences/LastChoosenWallpaper
